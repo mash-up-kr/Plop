df0eaeff5f43c0ecf50ddab72b7bc4e813ebaf2f
김혜원 <gpdnjs8151@naver.com>
2016-08-27 23:24:01 +0900

    MainViewController 화면 구성, MainViewTableViewCell 생성

---
+++ Plop/MainTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  MainTableViewCell.swift
+//  Plop
+//
+//  Created by 김혜원 on 2016. 8. 27..
+//  Copyright © 2016년 seolheelee. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @IBOutlet weak var yearLabel: UILabel!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var statusImageView: UIImageView!
+
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    
+}
